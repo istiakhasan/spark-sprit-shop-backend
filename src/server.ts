0b4b5e68c58214f0984ee6f14a74518eaa5d19b6
istiak <istiak@gmail.com>
2023-12-03 15:56:52 +0600

    abc

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+
+;(async () => {
+  try {
+    await mongoose.connect(config.database_url as string)
+
+    console.log(`Database is connected successfully`)
+    app.listen(config.port, () => {
+      console.log(`App listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`Failed to conntect database`, error)
+  }
+})()
