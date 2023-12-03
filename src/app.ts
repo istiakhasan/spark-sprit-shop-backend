2e53cde5b2edd2e08a0535117cad1197ca949c17
istiak <istiak@gmail.com>
2023-12-03 16:27:41 +0600

    abc

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-asdf 
+app.get('/', (req, res) => {
+  res.send('abc')
+})
 export default app
